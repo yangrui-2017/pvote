74e32a5462a88815772e59f271218538cf9dcb99
yr1216 <wangshuai@wangshuais-MacBook-Pro.local>
2013-09-13 10:59:44 +0100

    create sqlite
    create sqlite

--- Photo/classes/sqlService.h
+++ Photo/classes/sqlService.h
@@ -7,7 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
 
-@interface sqlService : NSObject
+#define KFilename @"testdb.sql"
+@class sqlTestList;
 
+@interface sqlService : NSObject{
+    
+    sqlite3 *_database;
+}
+
+@property (nonatomic) sqlite3 *_database;
+
+-(BOOL) createTestList:(sqlite3 *)db;//创建数据库
+-(BOOL) insertTestList:(sqlTestList *)insertList;//插入数据
+//-(BOOL) updateTestList:(sqlTestList *)updateList;//更新数据
+-(NSMutableArray*)getTestList;//获取全部数据
+- (BOOL) deleteTestList:(sqlTestList *)deletList;//删除数据：
+- (NSMutableArray*)searchTestList:(int)searchID;//查询数据库，searchID为要查询数据的ID，返回数据为查询到的数据
+@end
+
+@interface sqlTestList : NSObject
+{
+    
+}
+
+@property(retain,nonatomic) NSString * name;
+@property(retain,nonatomic) NSString * password;
 @end
