ff50db706b067af0ce76dab04d8c16111a0d2925
yr1216 <wangshuai@wangshuais-MacBook-Pro.local>
2013-10-09 13:08:49 +0100

    single image view controller
    single

---
+++ Photo/SingleImageViewController.h
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.h
+//  Photo
+//
+//  Created by wangshuai on 13-10-9.
+//  Copyright (c) 2013年 wangshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainViewController.h"
+
+@interface SingleImageViewController : MainViewController
+
+@end
---
+++ Photo/SingleImageViewController.m
@@ -0,0 +1,43 @@
+//
+//  SingleImageViewController.m
+//  Photo
+//
+//  Created by wangshuai on 13-10-9.
+//  Copyright (c) 2013年 wangshuai. All rights reserved.
+//
+
+#import "SingleImageViewController.h"
+
+@interface SingleImageViewController ()
+
+@end
+
+@implementation SingleImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)loadVotes{
+    
+    
+}
+
+@end
